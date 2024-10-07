1665510cad3be6a875660f50e4f56b91741783c0
Teltt <129358512+Teltt@users.noreply.github.com>
2024-10-07 03:32:22 -0600

    refactor: how Library scripts are loaded

--- addon/Library.gd
+++ addon/Library.gd
@@ -124,7 +124,7 @@ func export_class_list(script_info: Array,path) -> void:
 		file_path = ProjectSettings.localize_path(file_path)
 		file_path.replace(source_code_path,get_root())
 		file_path.replace(path,get_root())
-		file.store_line("const " + _class_name + " = \"" + file_path + "\"")
+		file.store_line("const " + _class_name + " = preload(\"" + file_path + "\")")
 	file.close()
 
 func delete(path: String) -> void:
